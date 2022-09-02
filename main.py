71a2f88d9408b5b94b700583c6e212226e3d51ba
wangyifa <112699212+wangyifa@users.noreply.github.com>
2022-09-02 22:37:46 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def get_ciba():
  
 def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
-    week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五7：50—9：30基金平", "星期六"]
+    week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五                  14：30-16：10论文写作   北区 逸夫楼130", "星期六"]
     year = localtime().tm_year
     month = localtime().tm_mon
     day = localtime().tm_mday
